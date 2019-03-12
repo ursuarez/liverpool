c7bf9f4a3b0bec3525c45611e4f1f2342d3e77d1
Ricardo Suarez <rsuarez@grupoasesores.com.mx>
2019-03-11 21:00:54 -0600

    Updates

---
+++ precache-manifest.d76b81663eceb6503d93d04a2bde1888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c7db9d3934d30a5a88a211af941dc2",
+    "url": "/liverpool/static/media/liverpool-logo.22c7db9d.png"
+  },
+  {
+    "revision": "d7fd12edfb6bf3411d23",
+    "url": "/liverpool/static/js/runtime~main.ff2ee37f.js"
+  },
+  {
+    "revision": "fc78954aaa3d53f2e47f",
+    "url": "/liverpool/static/js/main.aa745442.chunk.js"
+  },
+  {
+    "revision": "233bec75958951eb1430",
+    "url": "/liverpool/static/js/2.7a58f4ef.chunk.js"
+  },
+  {
+    "revision": "fc78954aaa3d53f2e47f",
+    "url": "/liverpool/static/css/main.fc7986f9.chunk.css"
+  },
+  {
+    "revision": "ddec74b1ded6d7b3b40178acf41adae4",
+    "url": "/liverpool/index.html"
+  }
+];
